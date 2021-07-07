b890c16836058d9642835dc3706b24413377538a
Amanda Torres <40478097+AmandaTN@users.noreply.github.com>
2021-07-07 00:16:03 -0500

    Last commit :)

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { mockPokemonData } from "../mock/pokeData";
 
-export default class PokeCard extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{mockPokemonData.name}</h1>
-        <img src={props.imgSrc} />
-        <img src={mockPokemonData.sprites.front_shiny} />
-        <br />
-        <a href={mockPokemonData.video}>Click me</a>
-      </div>
-    );
-  }
+export default function PokeCard(props) {
+  return (
+    <div key={props.index}>
+      <h1>{props.name}</h1>
+      <a target="_blank" rel="noreferrer" href={props.videoLink}>
+        <img
+          alt={props.name}
+          src={props.imgEnterSrc}
+          onMouseOver={(e) => (e.currentTarget.src = props.imgOverSrc)}
+          onMouseOut={(e) => (e.currentTarget.src = props.imgEnterSrc)}
+        />
+      </a>
+    </div>
+  );
 }
--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import PokeCard from "./PokeCard";
 import { mockPokemonsData } from "../mock/pokeData";
 
-export default class PokeCard extends React.Component {
+export default class PokeCards extends React.Component {
   render() {
-    return mockPokemonsData.map((value, i) => {
-      return (
-        <div key={i}>
-          <h1>{value.name}</h1>
-          <a target="_blank" src={value.video}>
-            <img
-              src={value.sprites.front_default}
-              onMouseOver={(e) =>
-                (e.currentTarget.src = Object.keys(value.sprites)[0])
-              }
-              onMouseOut={(e) =>
-                (e.currentTarget.src = value.sprites.front_default)
-              }
-            />
-          </a>
-        </div>
-      );
-    });
+    return mockPokemonsData
+      .sort((a, b) => (a.value - b.value ? 1 : -1)) //I did it descending because data is already ascending
+      .map((value, i) => {
+        let imgSrc = value.sprites;
+        let imgLink = (obj, index) => {
+          return obj[Object.keys(obj)[index]];
+        };
+
+        let props = {
+          index: i,
+          name: value.name,
+          imgEnterSrc: imgLink(imgSrc, 0),
+          imgOverSrc: imgLink(imgSrc, 1),
+          videoLink: value.videoLink
+        };
+
+        return <PokeCard {...props} />;
+      });
   }
 }
